1688830a89b86b9e07792c27d60839242672660c
Wajih Ud Din Kamran <wajihuddinkamran@users.noreply.github.com>
2020-12-05 15:36:09 +0500

    Commit 4: Adding screens of level one and level two

---
+++ sessional_2/lib/MainScreen.dart
@@ -0,0 +1,52 @@
+
+import 'package:flutter/material.dart';
+import 'secondscreen.dart';
+import 'thirdscreen.dart';
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        // leading: IconButton(icon: Icon(Icons.menu), onPressed: (){
+        //
+        // }),
+        title: Text("SELECT LEVEL"),
+      ),
+
+      body: Center(child: RaisedButton(
+
+        child: Text('Level 1',style: TextStyle(
+          color: Colors.cyanAccent,
+
+        ),
+        ),
+        color: Colors.blue,
+        onPressed: (){
+          Navigator.push(context,
+            MaterialPageRoute(builder: (context)=>SecondScreen()
+            ),
+          );
+        },
+      ),
+      ),
+
+      body: Center(child: RaisedButton(
+
+        child: Text('Level 1',style: TextStyle(
+          color: Colors.cyanAccent,
+
+        ),
+        ),
+        color: Colors.blue,
+        onPressed: (){
+          Navigator.push(context,
+          MaterialPageRoute(builder: (context)=>ThirdScreen()
+          ),
+          );
+        },
+      ),
+      ),
+    );
+  }
+}
---
+++ sessional_2/lib/secondscreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        // leading: IconButton(icon: Icon(Icons.menu), onPressed: (){
+        //
+        // }),
+        title: Text("Level 1"),
+      ),
+    );
+
+
+
+  }
+}
